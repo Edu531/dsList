7e349ac95f1ce83deb95747b81ea4b02a9c46b16
Eduardo Henrique Ferreira <dudu53188@gmail.com>
2023-05-14 17:50:09 -0300

    Game seed.

--- src/main/java/com/ferreiratech/dsList/entities/Game.java
+++ src/main/java/com/ferreiratech/dsList/entities/Game.java
@@ -19,25 +19,41 @@ public class Game {
 
     private String genre;
 
-    private String platform;
+    private String platforms;
 
-    private String imgURL;
+    private Double score;
 
+    private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
 
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
 
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgURL, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
-        this.imgURL = imgURL;
+        this.platforms = platforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl, String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platforms = platforms;
+        this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
@@ -74,20 +90,28 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
-    public String getImgURL() {
-        return imgURL;
+    public Double getScore() {
+        return score;
     }
 
-    public void setImgURL(String imgURL) {
-        this.imgURL = imgURL;
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
     }
 
     public String getShortDescription() {
